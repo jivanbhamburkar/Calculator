d702587efb8472c5ff7eff71422ace0545585257
jivanbhamburkar <96575075+jivanbhamburkar@users.noreply.github.com>
2022-03-23 18:17:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,27 @@ const display = document.getElementById("input");
 //functions
 
 function clickButton(val) {
-  display.value = display.value + val;
+  if (display.value == "Infinity") {
+    reset();
+  }
+
+  if (display.value.includes(".") && val == ".") {
+    return;
+  } else {
+    display.value = display.value + val;
+  }
 }
 
 function calc() {
-  display.value = eval(display.value);
+  if (eval(display.value) != display.value) {
+    display.value = eval(display.value);
+  }
 }
 
 function reset() {
   display.value = "";
 }
 
-function del(){
-    display.value = display.value.slice(0, -1);
+function del() {
+  display.value = display.value.slice(0, -1);
 }
